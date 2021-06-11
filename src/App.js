90307d854ddba7431371b697bf6c7e125bc891ec
fayez-als <fayez-als@live.com>
2021-06-11 17:05:58 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useContext, useState } from 'react';
+import { TextField } from '@material-ui/core';
+import axios from 'axios';
+
+
 
 function App() {
+
+  const [url,setURL] = useState("")
+
+ function handleSubmit(e){
+    
+    e.preventDefault()
+    console.log(url)
+    predict()
+    
+    
+    setURL("")
+  }
+
+
+
+  async function predict(){
+        let res = await axios.post('ec2-18-219-138-9.us-east-2.compute.amazonaws.com:5000/predict',{"url":url}) 
+
+        console.log(res)}
+
+
+
+  function handleChange(e){
+    setURL(e.target.value)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
+      <div className='form'>
+        <form onSubmit={handleSubmit}>
+          <TextField onChange={handleChange} value={url}/>
+          <button>Submit</button>
+
+        </form>
+
+      </div>
+      
+
+
+      
     </div>
   );
 }
